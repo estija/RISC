838274464fd4ca439399603ddbb0845563ad4133
estija <30321205+estija@users.noreply.github.com>
2017-10-07 13:07:32 +0530

    Update Control-Unit.v

--- Testbench/Control-Unit.v
+++ Testbench/Control-Unit.v
@@ -20,9 +20,9 @@
 //////////////////////////////////////////////////////////////////////////////////
 module test;
 reg[5:0]opcode;
-wire reg_src,reg_wen,alu_src,i_type,w_src,mem_wen;
+wire reg_src,reg_wen,alu_src,w_src,mem_wen,check_immed;
 wire [1:0]alu_cnt,pc_cnt;
-control uut(opcode,reg_src,reg_wen,alu_src,i_type,w_src,mem_wen,alu_cnt,pc_cnt);
+control uut(opcode,reg_src,reg_wen,alu_src,w_src,mem_wen,check_immed,alu_cnt,pc_cnt);
 initial begin
 opcode=6'b000000;
 end
