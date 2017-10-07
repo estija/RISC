de956cddc79e5b3f3e318a271747a784aaa0f0b6
estija <30321205+estija@users.noreply.github.com>
2017-10-07 12:39:18 +0530

    Create Data-Mem.v

---
+++ Testbench/Data-Mem.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09/22/2017 07:07:09 PM
+// Design Name: 
+// Module Name: dm_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module dm_test;
+reg[31:0]data_in,add;
+reg clk,wen;
+wire[31:0]data_out;
+data_mem uut(add,data_in,clk,wen,data_out);
+initial begin
+clk=0;
+data_in=32'd12;
+add=32'd2;
+wen=1;
+end
+always begin
+#10 clk=~clk;
+#10 wen=~wen;
+data_in=data_in+1;
+add=add+1;
+end
+endmodule
